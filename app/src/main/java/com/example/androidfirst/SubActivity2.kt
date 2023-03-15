37b13abec18cee088f50f5fd5a66980b7222e5ff
Kim JuSeong <kgkl1010@gmail.com>
2023-03-15 17:51:17 +0900

    [update] forth work

--- app/src/main/java/com/example/androidfirst/SubActivity2.kt
+++ app/src/main/java/com/example/androidfirst/SubActivity2.kt
@@ -2,10 +2,14 @@ package com.example.androidfirst
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.androidfirst.databinding.ActivitySub3Binding
 
 class SubActivity2 : AppCompatActivity() {
+    lateinit var binding: ActivitySub3Binding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sub3)
+        binding = ActivitySub3Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btnName.text = "문자열"
     }
 }
