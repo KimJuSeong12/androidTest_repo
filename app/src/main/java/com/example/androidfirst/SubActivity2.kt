8b6a3f6532409376f136c7575baa80f1f700c740
Kim JuSeong <kgkl1010@gmail.com>
2023-03-15 16:39:38 +0900

    [update] second work

---
+++ app/src/main/java/com/example/androidfirst/SubActivity2.kt
@@ -0,0 +1,11 @@
+package com.example.androidfirst
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SubActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sub4)
+    }
+}
