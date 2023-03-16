e327c9eeb8910de22c994822d638d070a7c525d6
Kim JuSeong <kgkl1010@gmail.com>
2023-03-16 17:36:53 +0900

    [update] fifth work

--- app/src/main/java/com/example/androidfirst/SubActivity2.kt
+++ app/src/main/java/com/example/androidfirst/SubActivity2.kt
@@ -2,14 +2,45 @@ package com.example.androidfirst
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.androidfirst.databinding.ActivitySub3Binding
+import com.example.androidfirst.databinding.*
 
 class SubActivity2 : AppCompatActivity() {
     lateinit var binding: ActivitySub3Binding
+    lateinit var binding2: ActivitySub11Binding
+    lateinit var binding3: ActivitySub13Binding
+    lateinit var binding4: ActivitySub15Binding
+    lateinit var binding5: ActivitySub16Binding
+    var flag: Boolean = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySub3Binding.inflate(layoutInflater)
-        setContentView(binding.root)
-        binding.btnName.text = "문자열"
+        binding2 = ActivitySub11Binding.inflate(layoutInflater)
+        binding3 = ActivitySub13Binding.inflate(layoutInflater)
+        binding4 = ActivitySub15Binding.inflate(layoutInflater)
+        binding5 = ActivitySub16Binding.inflate(layoutInflater)
+        setContentView(binding5.root)
+
+//            binding5.edtPassword.setOnLongClickListener {
+//                binding5.edtPassword.inputType = InputType.TYPE_TEXT_VARIATION_NORMAL
+//                Toast.makeText(applicationContext,binding5.edtPassword.text.toString(),Toast.LENGTH_SHORT).show()
+//
+//                return@setOnLongClickListener(true)
+//            }
+//        binding5.edtPassword.setOnClickListener{
+//            binding5.edtPassword.inputType = InputType.TYPE_TEXT_VARIATION_PASSWORD
+//        }
+
+//        binding3.btnClick.setOnClickListener {
+//            if (flag == true) {
+//                binding3.btnTarget.visibility = View.VISIBLE
+//                binding3.btnGone.visibility = View.VISIBLE
+//                flag = false
+//            } else {
+//                binding3.btnTarget.visibility = View.INVISIBLE
+//                binding3.btnGone.visibility = View.GONE
+//                flag = true
+//
+//            }
+//        }
     }
 }
